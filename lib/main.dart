4157edba6effd76037a5db62111da087b14d8daa
Ahmed Allam <ahmed.allam@izam.com>
2025-05-20 19:52:20 +0300

    Day10:

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,30 @@
+import 'package:first_project/utils/my_app_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(HomeScreen());
+  runApp(MyApp());
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-            child: Column(
-          children: [
-            SizedBox(
-              height: 150,
-            ),
-            Image.network(
-              "https://img.freepik.com/free-vector/login-concept-illustration_114360-739.jpg",
-              width: 200,
-              height: 200,
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Text(
-              "Hello",
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              "Welcome to my App, You will be happy of it",
-              style: TextStyle(color: Colors.grey),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Colors.blue,
-              ),
-              child: Center(
-                child: Text(
-                  "Login",
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.blueAccent,),
-                borderRadius: BorderRadius.circular(30),
-              ),
-              height: 50,
-              width: 250,
-              child: Center(
-                child: Text(
-                  "Sign Up",
-                  style: TextStyle(color: Colors.blueAccent, fontSize: 18),
-                ),
-              ),
-            ),
-          ],
-        )),
+        appBar: getMyAppBar(),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
 
 
 
---
+++ lib/utils/my_app_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+AppBar getMyAppBar(){
+  return AppBar(
+    backgroundColor: Colors.blue,
+    title: Text(
+      "WhatsApp",
+      style: TextStyle(color: Colors.white),
+    ),
+    actions: [
+      Padding(
+        padding: const EdgeInsets.only(right: 10),
+        child: Icon(
+          Icons.camera_alt_outlined,
+          color: Colors.white,
+        ),
+      ),
+      Icon(
+        Icons.search,
+        color: Colors.white,
+      ),
+      SizedBox(
+        width: 10,
+      ),
+      Icon(
+        Icons.more_vert,
+        color: Colors.white,
+      ),
+      SizedBox(
+        width: 10,
+      ),
+    ],
+  );
+}
